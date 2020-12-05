5f491cc74326101660dd30af1867846e94a4a5e8
CoDingDongHyuk <kderr2791@gmail.com>
2020-12-05 12:31:54 +0900

    Cors 처리

---
+++ src/main/kotlin/com/donghyukki/korello/core/web/WebConfig.kt
@@ -0,0 +1,16 @@
+package com.donghyukki.korello.core.web
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig: WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "DELETE", "PUT")
+            .maxAge(3000)
+    }
+}
