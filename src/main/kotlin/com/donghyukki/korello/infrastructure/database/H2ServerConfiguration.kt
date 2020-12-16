c8aa17d0996bb55c4e25332880623992ea80ec41
CoDingDongHyuk <kderr2791@gmail.com>
2020-12-16 23:31:47 +0900

    Import 수정

--- src/main/kotlin/com/donghyukki/korello/infrastructure/database/H2ServerConfiguration.kt
+++ src/main/kotlin/com/donghyukki/korello/infrastructure/database/H2ServerConfiguration.kt
@@ -5,6 +5,7 @@ import org.h2.tools.Server
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 
 @Profile("local")
 @Configuration
