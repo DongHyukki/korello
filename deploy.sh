1b36322e65029e15a3baadb7dc5ee1b75303683d
CoDingDongHyuk <kderr2791@gmail.com>
2020-12-17 00:12:58 +0900

    Deploy Shell 생성

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+HOST="donghyukki@192.168.1.15"
+./gradlew clean  bootJar
+sshpass -f /Users/kakao_ent/study/password.txt scp -r build/libs/*.jar $HOST:/home/donghyukki
+sshpass -f /Users/kakao_ent/study/password.txt ssh $HOST "cd /home/donghyukki; ./start.sh"
+exit 0;
