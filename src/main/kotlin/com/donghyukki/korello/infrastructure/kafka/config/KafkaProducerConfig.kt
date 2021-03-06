95b060b200db10728d7c21e19311c760fdf678cf
CoDingDongHyuk <kderr2791@gmail.com>
2021-03-06 22:10:24 +0900

    test

--- src/main/kotlin/com/donghyukki/korello/infrastructure/kafka/config/KafkaProducerConfig.kt
+++ src/main/kotlin/com/donghyukki/korello/infrastructure/kafka/config/KafkaProducerConfig.kt
@@ -5,6 +5,7 @@ import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.serialization.StringSerializer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.kafka.core.DefaultKafkaProducerFactory
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.support.serializer.JsonSerializer
